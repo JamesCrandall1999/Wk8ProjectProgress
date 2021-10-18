fe53fe066e20f27cf06f7dab5aee70b17ffecc77
JamesCrandall1999 <90048538+JamesCrandall1999@users.noreply.github.com>
2021-10-17 19:45:40 -0700

    Add files via upload

---
+++ Final Project/src/com/company/controllers/Main.java
@@ -0,0 +1,28 @@
+package com.company.controllers;
+
+import com.company.views.UserView;
+
+public class Main {
+    public static UserView view;
+    public static String choice;
+    public static int currentIdentity;
+    public static String currentName;
+    public static void main(String[] args) {
+
+       view = new UserView();
+       choice = view.startMenu();
+       switch(choice){
+           case "1":
+               currentIdentity = view.login();
+                break;
+           case "2":
+               currentName = view.createUser();
+               break;
+           case "3":
+               view.exit();
+               break;
+           default:
+               view.invalidchoice1();
+       }
+    }
+}
